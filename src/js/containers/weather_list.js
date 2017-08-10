07a015c65182421ebf0630697333e4d3ae98913a
JasonLinn <cheng11220@gmail.com>
2017-08-10 23:39:39 +0800

    finished

--- src/js/containers/weather_list.js
+++ src/js/containers/weather_list.js
@@ -1,20 +1,25 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
-import { Sparklines,SparklinesLine} from 'react-sparklines';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map'
+
 class WeatherList extends Component{
     renderWeather(cityData){
         const name = cityData.city.name;
         const temps = cityData.list.map(weather=>weather.main.temp);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+        const {lon,lat} = cityData = cityData.city.coord;
+        
         console.log(temps);
+        
+
         return (
             <tr key={name}>
-                <td>
-                    {name}
-                </td>
-                <td>
-                    <Sparklines height={120} width={180} data={temps}>
-                    
-                </td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td><Chart data={temps} units="K" color="orange"/></td>
+                <td><Chart data={pressure} units="hPa" color="black"/></td>
+                <td><Chart data={humidity} units="%" color="green"/></td>
             </tr>
         )
     }
@@ -25,9 +30,9 @@ class WeatherList extends Component{
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
--- src/js/reducers/reducer_weather.js
+++ src/js/reducers/reducer_weather.js
@@ -3,7 +3,8 @@ export default function (state=[],action){
     switch (action.type) {
         case 'FETCH_WEATHER':
             // return state.concat([action.payload.data]);
-            return [action.payload.data,...state];//[city,city,city]
+            //no state.tomorrowWheather = weather
+            return [action.payload.data,...state];//[city,city,city]//有新增的話會創新的陣列
     }
     return state;
 };
